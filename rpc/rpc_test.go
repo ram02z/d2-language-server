e5d0b7ff9813d058f87c6501013c705a559bb8b6
Omar Zeghouani <omarzeghouanii@gmail.com>
2024-07-14 15:00:24 +0100

    Fix RPC test

--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -11,7 +11,7 @@ type EncodingExample struct {
 
 func TestEncode(t *testing.T)  {
 	expected := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
-	actual := rpc.EncodeMessage(EncodingExample{Testing:true})
+	actual, _ := rpc.EncodeMessage(EncodingExample{Testing:true})
 
 	if expected != actual {
 		t.Fatalf("Expected: %s, Actual %s", expected, actual)
